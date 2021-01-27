1570696eb7e8b6b902f09ff0837cd31e557de008
TejasOS <tejasos100@gmail.com>
2021-01-27 09:22:20 -0800

    Updated SelectionSort

--- SelectionSort.py
+++ SelectionSort.py
@@ -2,18 +2,20 @@
 
 def selectionSort(arrToBeSorted):
     toBeSwitched = None
-    minNum = 0
-    for i in range(len(arrToBeSorted)): # For each element in array to be sorted, loop through array and see what minimum value is after that element
-        for j in range(i, len(arrToBeSorted)):
+    # For each element in array to be sorted, loop through array and see what minimum value is after that element
+    for i in range(len(arrToBeSorted)):
+        minNum = i
+        for j in range(i+1, len(arrToBeSorted)):
             if (arrToBeSorted[j] < arrToBeSorted[minNum]):
                 minNum = j
 
-        toBeSwitched = arrToBeSorted[i] # Get current element at position that min needs to be switched to
-        arrToBeSorted[i] = arrToBeSorted[minNum] # Move min num to correct pos (e.g. second smallest number would be moved to index of 1)
-        arrToBeSorted[minNum] = toBeSwitched # Fill the empty space with original num
-        minNum = i+1 # Set the minimum number pos to where we start looping again
+        # Get current element at position that min needs to be switched to
+        toBeSwitched = arrToBeSorted[i]
+        # Move min num to correct pos (e.g. second smallest number would be moved to index of 1)
+        arrToBeSorted[i] = arrToBeSorted[minNum]
+        # Fill the empty space with original num
+        arrToBeSorted[minNum] = toBeSwitched
     return arrToBeSorted
 
 
 print(selectionSort([3, 1, 2]))
-
